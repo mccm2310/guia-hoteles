4037519a79dc4744ea6992170212109395d4e4d5
Mirlet Campos <mccm2310@gmail.com>
2019-07-17 15:06:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-weather'));
+app.use(express.static('./dist/guia-hoteles'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/angular-weather/index.html'));
+res.sendFile(path.join(__dirname,'/dist/guia-hoteles/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
